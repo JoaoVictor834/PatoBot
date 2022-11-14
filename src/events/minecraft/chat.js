fbdac6fbadf5d78acc2f47f5848ae16c1faf30b7
JoaoVictor834 <jv0888854@gmail.com>
2022-11-14 15:12:03 -0400

    Comandos do minecraft

--- src/events/minecraft/chat.js
+++ src/events/minecraft/chat.js
@@ -34,7 +34,7 @@ module.exports = class extends Event.mEvent {
             else {
                 try {
                 cmd = 
-                this.ebot.commands.foreach(c => c.aliases.foreach(a => a.find(r => r.test(message)))) ||
+                this.ebot.commands.forEach(c => c.aliases.forEach(a => a.find(r => r.test(message)))) ||
                 this.ebot.commands.find(c => c.name.find(r => r.test(message)))
                 } catch (err) {
                     console.log(err)
