a0550205ba84bccf94cbc3cd0493fe653b8c806f
JoaoVictor834 <jv0888854@gmail.com>
2022-11-14 15:45:02 -0400

    Comandos do minecraft

--- src/events/minecraft/chat.js
+++ src/events/minecraft/chat.js
@@ -14,18 +14,19 @@ module.exports = class extends Event.mEvent {
         if(username === this.bot.username) return
         
         
-        if(message.startsWith(prefix)) {
+       
             const args = message.slice(1).trim().split(/ +/g);
             const command = args.shift().toLowerCase();
             
-            const cmd = 
-            this.ebot.commands.find(c => c.name === command) || 
-            this.ebot.commands.find(c => c.aliases.find(a => a === command)) ||
-            this.ebot.commands.find(c => c.aliases.find(a => a.test(message))) || 
-            this.ebot.commands.find(c => c.name.find(n => n.test(message)))
+            const cmd = message.startsWith(prefix) ?
+            
+            this.ebot.commands.find(c => c.name === command) ||
+            this.ebot.commands.find(c => c.aliases.find(a => a === command)) :
+            this.ebot.commands.forEach(c => c.aliases.find(a => a.test(message))) || 
+            this.ebot.commands.forEach(c => c.name.find(n => n.test(message)))
 
-            if(cmd) return cmd.run(message, username, args)
-        }
+            if(cmd) return cmd.run(username, message, args)
+        
 
 
 
