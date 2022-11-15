574248b65b2d730836cdbafee732e4a4931e146f
Pexxxzz <96544669+JoaoVictor834@users.noreply.github.com>
2022-11-15 18:45:17 -0400

    Update Bot.js

--- src/structures/Bot.js
+++ src/structures/Bot.js
@@ -2,6 +2,7 @@ const { readdirSync } = require('fs')
 const { join } = require('path')
 const mineflayer = require('mineflayer')
 var tpsPlugin = require('mineflayer-tps')(mineflayer)
+var radarPlugin = require(mineflayer-radar)(mineflayer)
 const filterlist = require('../../filter.json')
 const Filter = require('badwords-filter')
 
@@ -25,6 +26,7 @@ const filterconfig = {
         this.updateChatPatern()
         
         bot.loadPlugin(tpsPlugin)
+        radarPlugin(this.bot)
         
          this.filter = new Filter(filterconfig)
 
