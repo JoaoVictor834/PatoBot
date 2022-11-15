8c4bfe90de7a7b2e544d30e439890468ab45e78a
Pexxxzz <96544669+JoaoVictor834@users.noreply.github.com>
2022-11-15 16:53:18 -0400

    Update coordleak.js

--- src/commands/minecraft/coordleak.js
+++ src/commands/minecraft/coordleak.js
@@ -9,6 +9,9 @@ module.exports = class extends Command.mCommand {
     }
     run = (_, i, args) => {
 
+if(!args[0]) return this.bot.chat(`Me fale de quem você quer as cordenadas!`)
+
+
         this.bot.chat(`Cordenadas de ${args[0]}: ${Math.round(Math.random() * 130308)}, ${Math.round(Math.random() * 40380)}`)
 
     }
