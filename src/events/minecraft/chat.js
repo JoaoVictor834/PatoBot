7cbe037d7f4036aecf6b48967a5bf215d0ba8ea1
Pexxxzz <96544669+JoaoVictor834@users.noreply.github.com>
2022-11-15 10:01:39 -0400

    Update chat.js

--- src/events/minecraft/chat.js
+++ src/events/minecraft/chat.js
@@ -18,21 +18,24 @@ module.exports = class extends Event.mEvent {
             const args = message.slice(1).trim().split(/ +/g);
             const command = args.shift().toLowerCase();
 
-            let cmd
+           
             
-            if(this.ebot.commands.find(c => c.aliases !== undefined)) {
-                this.ebot.commands.find(c => c.aliases.forEach(a => console.log(a)))
-            }
+            
 
 
-            cmd = this.ebot.commands.find(c => c.aliases === undefined) ?
+           const cmd = message.startsWith(prefix) ?
 
             this.ebot.commands.find(c => c.name === command) ||
-            this.ebot.commands.forEach(c => c.name.find(n => n.test(message))) :
+            this.ebot.commands.forEach(c => { 
+if(c.aliases === undefined) return
+return c.aliases.find(a => a === command)
+}) :
 
-            this.ebot.commands.find(c => c.aliases.find(a => a === command)) ||
-            this.ebot.commands.forEach(c => c.aliases.find(a => a.test(message)))
-
+            this.ebot.commands.forEach(c => {
+if(c.aliases === undefined) return
+c.aliases.forEach(a => console.log(a)
+}) ||
+            this.ebot.commands.forEach(c => c.name.find(n => console.log(n))
 
             if(cmd) return cmd.run(username, message, args)
         
