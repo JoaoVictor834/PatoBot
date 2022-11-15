6bc9056b890e7f6e06419c03bb7027323d9b36e3
Pexxxzz <96544669+JoaoVictor834@users.noreply.github.com>
2022-11-15 18:49:31 -0400

    Update Bot.js

--- src/structures/Bot.js
+++ src/structures/Bot.js
@@ -2,7 +2,7 @@ const { readdirSync } = require('fs')
 const { join } = require('path')
 const mineflayer = require('mineflayer')
 var tpsPlugin = require('mineflayer-tps')(mineflayer)
-var radarPlugin = require(mineflayer-radar)(mineflayer)
+var radarPlugin = require('mineflayer-radar')(mineflayer)
 const filterlist = require('../../filter.json')
 const Filter = require('badwords-filter')
 
