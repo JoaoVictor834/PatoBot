e5f462265307d3dde30d34a8fe19629e807d0606
Pexxxzz <96544669+JoaoVictor834@users.noreply.github.com>
2022-11-30 22:44:37 -0400

    Update chat.js

--- src/events/minecraft/chat.js
+++ src/events/minecraft/chat.js
@@ -91,7 +91,7 @@ module.exports = class extends Event.mEvent {
                             {
                                 content: this.ebot.filter.clean(message) || '\`Mensagem inválida\`',
                                 username: username || 'Nome invalido',
-                                avatarURL: uuid ? `https://crafatar.com/avatars/${uuid}?size=32&overlay` : await RandomAvatar() : await getAvatar()
+                                avatarURL: await getAvatar() ? await getAvatar().catch(e => console.log(e)) : uuid ? `https://crafatar.com/avatars/${uuid}?size=32&overlay` : await RandomAvatar() 
 
                             }).catch(e => {
                        console.log(e)
