07980a2279d9fbe65b487f620b04625d76910ac2
Duckz <96544669+JoaoVictor834@users.noreply.github.com>
2022-12-11 11:02:44 -0400

    Update antispam.js

--- src/antispam.js
+++ src/antispam.js
@@ -132,7 +132,7 @@ dcmsgs.push({
 })
 
 if(dcmsgs.lenght >=10) return dcmsgs = []
-console.log(dcmsgs)
+console.log("Dcmsgs value: " + dcmsgs.val)
       
 
 
@@ -175,7 +175,7 @@ dcmsgs.push({
 })
 
 if(dcmsgs.lenght >=10) return dcmsgs = []
-console.log(dcmsgs)
+console.log("Dcmsgs value: " + dcmsgs.val)
 
       
 
@@ -255,7 +255,7 @@ deathmsgs.push({
 })
 
 if(deathmsgs.lenght >=10) return deathmsgs = []
-console.log(deathmsgs)
+console.log("DeathsMsg value: " + deathmsgs.dval)
       
 
 
@@ -290,7 +290,7 @@ deathmsgs.push({
 })
 
 if(deathmsgs.lenght >=10) return deathmsgs = []
-console.log(deathmsgs)
+console.log("DeathsMsg value: " + deathmsgs.dval
 
       
 
