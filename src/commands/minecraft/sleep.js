b5a32ed359066da679d83c167a136a74e63f977d
Duckz <96544669+JoaoVictor834@users.noreply.github.com>
2022-12-13 17:40:13 -0400

    Update sleep.js

--- src/commands/minecraft/sleep.js
+++ src/commands/minecraft/sleep.js
@@ -10,7 +10,7 @@ module.exports = class extends Command.mCommand {
 
        if(this.bot.time.isDay && this.bot.thunderState === 0) return this.bot.chat('Este comando so fuciona de noite ou em tempestades!')
 
-       if(Object.keys(this.bot.players).length > 1) return this.bot.chat('Este comando so fuciona quando somente 1 pessoa esta online!')
+       if(Object.keys(this.bot.players).length > 2) return this.bot.chat('Este comando so fuciona quando somente 1 pessoa esta online!')
 
        this.bot.chat('Ta bom vou deixar voce dormir :(')
        this.bot.quit('Alguem ta querendo dormir...')
