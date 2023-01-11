ccbc572e44e66236e48ff3bd7853d1ef5c48308d
Duckz <96544669+JoaoVictor834@users.noreply.github.com>
2023-01-10 21:10:38 -0400

    Change Id verification to permission verification

--- src/commands/discord/cmd.js
+++ src/commands/discord/cmd.js
@@ -1,5 +1,6 @@
 const Command = require('../../structures/Command')
 const wait = require('node:timers/promises').setTimeout
+const { PermissionsBitField } = require('discord.js');
 
 
 module.exports = class extends Command.dCommand {
@@ -54,7 +55,7 @@ module.exports = class extends Command.dCommand {
 
         const command = interaction.options.getString('comando')
 
-        if (command.startsWith('prefix') || command.startsWith('queue') || command.startsWith('ignore') || command.startsWith('party') && interaction.user.id !== '990061390029012992') return interaction.reply({ content: 'Comando bloqueado :rage:', ephemeral: true })
+        if (command.startsWith('prefix') || command.startsWith('queue') || command.startsWith('ignore') || command.startsWith('party') && !interaction.user.member.permissions.has(PermissionsBitField.flags.Administrator) return interaction.reply({ content: 'Comando bloqueado :rage:', ephemeral: true })
 
         if (command.startsWith('tell')) {
             let message = `${command} (Enviado por ${interaction.user.username})`
